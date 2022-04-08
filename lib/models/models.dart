9ef63bc6f1c75c2204557c66afea0bcafb250d8f
fikrimauladi123 <fikrimauladi23@yahoo.co.id>
2022-04-08 10:21:13 +0700

    frist comit

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -12,6 +12,6 @@ class DataModel {
 List<DataModel> dataList = [
   DataModel("Viqri Mauladi", "assets/images/viqri.jpeg", 136.8),
   DataModel("Viqri Mauladi", "assets/images/Viqri1.jpeg", 245.2),
-  DataModel("Casual Jeans", "assets/images/fashion4.png", 168.2),
-  DataModel("Jeans Skirt", "assets/images/fashion3.jpg", 136.7),
+  DataModel("Casual Jeans", "assets/images/hanif.jpeg", 168.2),
+  DataModel("Jeans Skirt", "assets/images/dafa.jpeg", 136.7),
 ];
